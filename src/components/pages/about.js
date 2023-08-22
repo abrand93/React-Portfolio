579dcad63246ebd01fee9d89dd3fa8c22608efad
abrand93 <125417439+abrand93@users.noreply.github.com>
2023-08-22 16:15:12 -0500

    Update about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -10,6 +10,9 @@ export default function About() {
       style={{width: "18rem"}}></img>
       <p> My name is Andrew Brandstrom. I’m starting my adventure in becoming a web developer. This is my
         portfolio. I'm a few weeks away from completing UofM coding bootcamp. 
+
+
+          
         //   I’m currently a production supervisor for Trafera. I’ve
         // worked there for 9 years. Once completing bootcamp I'm looking to make a caree change in to becoming a
         // developer.
